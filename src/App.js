64065f1ed71c1bbd4e27617676e6fbc567ea03ca
Sofia <m.sofiasarhiri@gmail.com>
2024-10-07 18:46:16 -0500

    installed react router paths

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import Navbar from '../src/pages/Navbar'
+import Home from '../src/pages/Home'
+import Menu from '../src/pages/Menu'
+import About from '../src/pages/About'
+import Contact from '../src/pages/Contact'
+
 
 function App() {
   return (
-    <div className="App">
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
