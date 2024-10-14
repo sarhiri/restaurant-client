cf0d978a2d2cb08ed53b488a1d158eb901fa6e24
Sofia <m.sofiasarhiri@gmail.com>
2024-10-14 15:03:02 -0500

    google maps through iframe

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,32 +1,32 @@
-import background from '../images/cilantro.jpg'
+import React from 'react';
 
-export default function Example() {
-  
-
+export default function MapWithText() {
   return (
-    <div className="bg-white">
-      <div className="flex flex-col justify-center">
-        <div className="border">
-        <h2 className="text-center font-semibold pt-10 text-red-600">ABOUT</h2>
+    <div className="flex flex-col lg:flex-row items-center justify-between max-w-7xl mx-auto py-16">
+      {/* Left Side - Google Maps */}
+      <div className="w-full lg:w-1/2 ">
+        <iframe
+          title="Google Maps"
+          width="100%"
+          height="100%"
+          loading="lazy"
+          allowFullScreen
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345093745!2d144.95592511531588!3d-37.817209979751554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf5779d46b6f7b13d!2sGoogle!5e0!3m2!1sen!2sus!4v1631020342356!5m2!1sen!2sus"
+          className="rounded-lg sm:px-20 "
+        ></iframe>
+      </div>
+
+      {/* Right Side - Text Content */}
+      <div className="w-full lg:w-1/2 lg:pl-12 pt-10 lg:pt-0">
+     
           <p className=" pb-5 text-2xl text-center font-bold tracking-tight text-gray-900 sm:text-4xl">Hours </p>
-        <div className=" pb-10  text-center">
+        <div className=" pb-10 mt-3 text-center">
           Monday - Thursday, 12pm - 9pm <br/>
           Friday - Saturday, 12pm - 11pm <br/>
           Sunday, Closed
 
         </div>
-        <div>
-          <p>Something goes ere</p>
-          <img
-        alt=""
-        src={background}
-        className=" -z-10 h-full w-full object-cover object-right md:object-center blur-extra-light"
-      />
-      {/* Semi-transparent overlay */}
-      <div className="absolute inset-0 bg-gray-900 opacity-50 -z-10"></div>
-        </div>
-        </div>
       </div>
     </div>
-  )
+  );
 }
