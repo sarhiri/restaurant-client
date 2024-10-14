7f1ea3752f1f1897cfe8cc294f26c572cf7b2aab
Sofia <m.sofiasarhiri@gmail.com>
2024-10-14 16:45:00 -0500

    MVP done

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,17 +1,15 @@
-import  { useState, useEffect } from 'react';
-import pic1 from '../images/slider/slider1.jpg'
-import pic2 from '../images/slider/slider2.jpg'
-import pic3 from '../images/slider/slider3.jpg'
-import pic5 from '../images/slider/slider4.jpg'
-import pic6 from '../images/slider/slider5.jpg'
+import { useState, useEffect } from 'react';
+import pic1 from '../images/slider/slider1.jpg';
+import pic2 from '../images/slider/slider2.jpg';
+import pic3 from '../images/slider/slider3.jpg';
+import pic5 from '../images/slider/slider4.jpg';
+import pic6 from '../images/slider/slider5.jpg';
 
 const Carousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const slides = [
-    pic1, pic2, pic3, pic5, pic6
-  ];
+  const slides = [pic1, pic2, pic3, pic5, pic6];
 
-  // Automatic slide change every 5 seconds
+  // Automatic slide change every 8 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
@@ -33,88 +31,88 @@ const Carousel = () => {
 
   return (
     <div className=''>
-    <div className="relative py-20" data-carousel="slide">
-      <div className="relative md:h-96 py-10 rounded-lg">
-        {slides.map((slide, index) => (
-          <div
-            key={index}
-            className={`absolute block w-full transition-opacity  duration-700 ease-in-out ${
-              currentSlide === index ? 'opacity-100' : 'opacity-0'
-            }`}
-          >
-            
-            <img
-              src={slide}
-              alt={`Slide ${index + 1}`}
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 "
-            />
-          <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
-          </div>
-        ))}
+      <div className="relative py-20" data-carousel="slide">
+        <div className="relative md:h-96 py-10 rounded-lg">
+          {slides.map((slide, index) => (
+            <div
+              key={index}
+              className={`absolute block w-full transition-opacity duration-700 ease-in-out ${
+                currentSlide === index ? 'opacity-100' : 'opacity-0'
+              }`}
+            >
+              <img
+                src={slide}
+                alt={`Slide ${index + 1}`}
+                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              />
+              {/* Dark overlay with higher z-index */}
+              <div className="absolute inset-0 bg-black opacity-50 z-40"></div>
+            </div>
+          ))}
+        </div>
+
+        {/* Slider indicators */}
+        <div className="absolute z-50 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+          {slides.map((_, index) => (
+            <button
+              key={index}
+              type="button"
+              className={`w-3 h-3 rounded-full ${currentSlide === index ? 'bg-red-500' : 'bg-gray-400'}`}
+              aria-label={`Slide ${index + 1}`}
+              onClick={() => goToSlide(index)}
+            ></button>
+          ))}
+        </div>
+
+        {/* Slider controls */}
+        <button
+          type="button"
+          className="absolute top-0 left-0 z-50 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={goToPreviousSlide}
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white">
+            <svg
+              className="w-4 h-4 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+
+        <button
+          type="button"
+          className="absolute top-0 right-0 z-50 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          onClick={goToNextSlide}
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white">
+            <svg
+              className="w-4 h-4 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 9l4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
       </div>
-
-      {/* Slider indicators */}
-      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        {slides.map((_, index) => (
-          <button
-            key={index}
-            type="button"
-            className={`w-3 h-3 rounded-full ${currentSlide === index ? 'bg-red-500' : 'bg-gray-400'}`}
-            aria-label={`Slide ${index + 1}`}
-            onClick={() => goToSlide(index)}
-          ></button>
-        ))}
-      </div>
-
-      {/* Slider controls */}
-      <button
-        type="button"
-        className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={goToPreviousSlide}
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white">
-          <svg
-            className="w-4 h-4 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-
-      <button
-        type="button"
-        className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={goToNextSlide}
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white">
-          <svg
-            className="w-4 h-4 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 9l4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
-    </div>
     </div>
   );
 };
