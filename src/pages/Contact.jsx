045f4ae8d93f05e344a322c83a93f0295cf10867
Sofia <m.sofiasarhiri@gmail.com>
2024-10-14 23:10:52 -0500

    sumbitted

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -170,7 +170,7 @@ export default function Example() {
                   </div>
                   <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                     <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                      Sumbitted 
+                      Submitted 
                     </DialogTitle>
                     <div className="mt-2">
                       <p className="text-sm text-gray-500">
