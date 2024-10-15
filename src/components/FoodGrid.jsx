f52060d783fefcb9908793400f886e80ee3f4894
Sofia <m.sofiasarhiri@gmail.com>
2024-10-15 13:27:45 -0500

    added order link

--- src/components/FoodGrid.jsx
+++ src/components/FoodGrid.jsx
@@ -64,7 +64,7 @@ export default function Example() {
       {/* Order button */}
             <div className='flex justify-center p-10 '>
       <a
-                href="/menu"
+                href="https://www.ubereats.com/"
                 className="rounded-md bg-red-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 tracking-wider"
               >
                 ORDER NOW
--- src/components/Mission2.jsx
+++ src/components/Mission2.jsx
@@ -19,7 +19,7 @@ export default function Example() {
               <div className="flex justify-center items-center p-20">
               <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="/menu"
+                href="https://www.ubereats.com/"
                 className="rounded-md bg-red-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 tracking-wider"
               >
                 ORDER NOW
